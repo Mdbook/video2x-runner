3cfe725e9409daf9de0f437c77b56810dc8c5731
mdbook <mikayla@mdbook.me>
2025-03-06 18:48:27 -0500

    added json todo

--- scripts/process.py
+++ scripts/process.py
@@ -5,6 +5,7 @@ import subprocess
 from pathlib import Path
 import ffmpeg
 
+# TODO add config json file
 # TODO resolve naming conflicts (error handling)
 # TODO add logging
 # TODO add support for multiple input formats (in progress)
